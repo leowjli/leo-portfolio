87468f0870d2a16e443bb9ee0c99a498e26cc874
leowjli <leo.wj.li@gmail.com>
2023-06-16 17:43:13 -0700

    moved images to public

--- src/components/ResumeCard.js
+++ src/components/ResumeCard.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
   export default function ResumeCard() {
     return (
       <section className="resume" >
-        <img className="resumepic" src="/img/Career Resume.png" alt="Leo's resume" />
+        <img className="resumepic" src="/img/Resume.png" alt="Leo's resume" />
         <button>
           <Link to="/img/Career Resume.pdf" download="Leo Li's Resume" target="_blank">Download PDF</Link>
         </button>
