0338e623344694824f6d1ed3bfeea0a63e2c095a
leowjli <leo.wj.li@gmail.com>
2023-06-19 17:07:25 -0700

    completed mobile-first css for all pages

--- src/components/InvolveCards.js
+++ src/components/InvolveCards.js
@@ -3,30 +3,24 @@ import { Link } from "react-router-dom";
 
   function Cards(props) {
     return (
-      <h5>
-        {props.position}
-        <Link to={props.link} target="_blank" className={props.org} >
-          <strong>{props.orgName}</strong>
-        </Link>
-      </h5>
+      <div>
+        <h5>
+          {props.position}
+          <Link to={props.link} target="_blank" className={props.org} >
+            <strong>{props.orgName}</strong>
+          </Link>
+        </h5>
+      </div>
     );
   }
 
   export default function InvolveCards() {
     return (
       <div className="activities">
-        <div>
-          <Cards position={"UX Design and Web Development for "} link={"https://myearthyourearth.com/"} orgName={"My Earth Your Earth"} />
-        </div>
-        <div>
-          <Cards position={"Officer and Performance Director at "} link={"https://www.csauw.com/"} org={"csa"} orgName={"Chinese Student Assoication"} />
-        </div>
-        <div>
-          <Cards position={"Hacker at "} link={"https://dh22.dubhacks.co/"} org={"dh"} orgName={"DubHacks Hackathon"} />
-        </div>
-        <div>
-          <Cards position={"Cymbals Player for the Husky Drumline and "} link={"https://www.huskymarchingband.org/"} org={"hmb"} orgName={"Husky Marching Band"} />
-        </div>
+        <Cards position={"UX Design and Web Development for "} link={"https://myearthyourearth.com/"} orgName={"My Earth Your Earth"} />
+        <Cards position={"Officer and Performance Director at "} link={"https://www.csauw.com/"} org={"csa"} orgName={"Chinese Student Assoication"} />
+        <Cards position={"Hacker at "} link={"https://dh22.dubhacks.co/"} org={"dh"} orgName={"DubHacks Hackathon"} />
+        <Cards position={"Cymbals Player for the Husky Drumline and "} link={"https://www.huskymarchingband.org/"} org={"hmb"} orgName={"Husky Marching Band"} />
       </div>
     );
  }
