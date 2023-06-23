528a092b5447fd6a20ac0d5d8bbc9c4c8012bff1
leowjli <leo.wj.li@gmail.com>
2023-06-22 22:04:36 -0700

    started adding social css

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -5,9 +5,10 @@ import React from "react";
       <section className="about">
         <img src="/img/IMG_6671.JPG" alt="me smiling with sunlight on my face casting a glow with beige background" />
         <div>
-          <h1>Great to meet you!</h1>
+          <p className="hi">Great to meet you!</p>
+          <h1 className="greet">I&apos;m Leo!</h1>
           <p>
-            I&apos;m Leo, a third year student from Canada studying Informatics and Math and computer-related topics at the University of Washington. I am passionate about the design and coding processes that connects information and people, and strive to create new applications or designs that positively impacts the world.
+            I&apos;m a third year student from Canada studying Informatics and Math and computer-related topics at the University of Washington. I am passionate about the design and coding processes that connects information and people, and strive to create new applications or designs that positively impacts the world.
           </p>
           <p>
             Currently, I am diving deeper into frontend development and have joined clubs that enhances my coding skills, working with peers to create projects that solve problems around campus.
--- src/components/Socials.js
+++ src/components/Socials.js
@@ -1,18 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
   function SocialsCard(props) {
     return (
-      <Link to={props.address} className={props.name} target="_blank" />
+      <li className="socials-items"><Link to={props.address} className={props.name} target="_blank" /></li>
     );
   }
 
   export default function Socials() {
+    const [openSocials, setOpenSocials] = useState(false);
+
+    function toggleButton() {
+      setOpenSocials(!openSocials);
+    }
+
     return (
-      <section className="socials">
-        <SocialsCard address={"mailto:leo.wj.li@gmail.com"} name={"gmail"} /><i className="fa-regular fa-envelope" />
-        <SocialsCard address={"https://www.linkedin.com/in/leoli07/"} name={"linkedin"} /><i className="fa-brands fa-linkedin-in" />
-        <SocialsCard address={"https://www.instagram.com/leo.268/"} name={"instagram"} /><i className="fa-brands fa-instagram" />
-      </section>
+      <div className="socials">
+        {/* <button className="share" onClick={toggleButton}><i className="fa-solid fa-share-nodes" /></button> */}
+        <ul className="socials-list">
+          <SocialsCard address={"mailto:leo.wj.li@gmail.com"} name={"gmail"} /><i className="fa-regular fa-envelope" />
+          <SocialsCard address={"https://www.linkedin.com/in/leoli07/"} name={"linkedin"} /><i className="fa-brands fa-linkedin-in" />
+          <SocialsCard address={"https://www.instagram.com/leo.268/"} name={"instagram"} /><i className="fa-brands fa-instagram" />
+        </ul>
+      </div>
     );
   }
