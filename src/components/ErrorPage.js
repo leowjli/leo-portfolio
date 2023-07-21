79f913aa08c7ced051ea4e07ebc4ee5cd021f950
leowjli <leo.wj.li@gmail.com>
2023-07-21 14:16:51 -0700

    added basic error page

---
+++ src/components/ErrorPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+  export default function ErrorPage() {
+    return (
+      <main className="d-flex flex-column align-items-center justify-content-center mt-5">
+        <h1>Uh oh! Can't find this one :<span>&#x28;</span></h1>
+        <p>try again!</p>
+        <img src="" alt="" />
+      </main>
+    );
+  }
