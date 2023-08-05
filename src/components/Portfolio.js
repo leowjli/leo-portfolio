59e3dc5dd03b13ffea1ecb81e866912c564e1e30
leowjli <leo.wj.li@gmail.com>
2023-08-05 16:44:50 -0700

    added genz project + updated design resume

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -2,6 +2,7 @@ import React from "react";
 import Banner from "./Banner";
 import EZParkCard from "./EZParkCard";
 import MeyeCard from "./MeyeCard";
+import GenZCard from "./GenZCard";
 
   export default function Portfolio() {
     return (
@@ -10,6 +11,7 @@ import MeyeCard from "./MeyeCard";
         <div className="work">
           <EZParkCard />
           <MeyeCard />
+          <GenZCard />
         </div>
       </main>
     );
